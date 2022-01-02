4283c46db5dd74cd98221cc83c262a4ee56398f0
pjp0704 <junhyeong25@gmail.com>
2022-01-02 21:24:57 +0900

    🍫 - Add commit executioner

---
+++ src/commands/commit/executeCommit.ts
@@ -0,0 +1,39 @@
+import chalk from "chalk";
+import { execSync } from "child_process";
+import configuration from "../../utils/configuration";
+
+interface Answer {
+  stageFiles?: string[];
+  emoji: string;
+  issue: string;
+  title: string;
+  message: string;
+}
+
+const stageFiles = (files: string[]) => {
+  execSync("git add " + files.join(" "));
+};
+
+export const executeCommit = (answer: Answer) => {
+  if (answer.stageFiles) stageFiles(answer.stageFiles);
+  try {
+    execSync(
+      `git commit -m "${answer.emoji}${
+        answer.issue ? ` (#${answer.issue})` : ""
+      } - ${answer.title}" ${answer.message ? `-m ${answer.message}` : ""} ${
+        configuration.getSignedCommit() ? "-S" : ""
+      }`
+    );
+    console.log(
+      "커밋 제목: " +
+        chalk.cyan(
+          `${answer.emoji}${answer.issue ? ` (#${answer.issue})` : ""} - ${
+            answer.title
+          }"`
+        )
+    );
+  } catch (e: any) {
+    console.error("커밋 중 오류 발생");
+    console.error(e.stdout.toString());
+  }
+};
