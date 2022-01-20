08b661af3f4788f47ef3455c4244297d168b05cf
pjp0704 <junhyeong25@gmail.com>
2022-01-20 22:57:34 +0900

    😎 - Fix that program don't bring up the issue number

--- src/commands/commit/checkIssueNumber.ts
+++ src/commands/commit/checkIssueNumber.ts
@@ -8,5 +8,5 @@ export const checkIssueNumber = (): string | undefined => {
     .filter((branch) => branch.startsWith("*"))[0]
     .substring(1);
 
-  return branch.match(/^([1-9]*)/)?.[0];
+  return /^\w*\/([1-9]*)/g.exec(branch)?.[1];
 };
